4b1df41a813cf4e469fc7c7490e8b3a968ed2330
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-02-21 23:52:57 +0500

    forgot password screen

--- lib/views/forgot_password_screen/forot_password_screen.dart
+++ lib/views/forgot_password_screen/forot_password_screen.dart
@@ -1,1 +1,10 @@
-stlessW
+import '../../constants/app_linker/app_linker.dart';
+
+class ForotPasswordScreen extends StatelessWidget {
+  const ForotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
