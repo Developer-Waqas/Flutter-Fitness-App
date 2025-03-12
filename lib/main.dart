1afbe5a7060821edaca08fb51472e539cf0697fa
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-03-12 13:11:15 +0500

    ✨ AppBar Complte

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Fitness App',
+      title: 'Fitness Pro',
       initialRoute: AppRoutes.splash,
       getPages: GenerateRoutes.getRoute(),
       debugShowCheckedModeBanner: false,
